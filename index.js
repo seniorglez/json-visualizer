987a49fdb82eade75ac01b02936100b59cc5eec1
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2021-01-05 16:28:25 +0100

    wipped Nadine send help

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
+// example https://github.com/urbandataanalytics/bento-components/blob/develop/src/components/Table/Table.stories.js
+
+
 import * as React from 'react';
 import reactdom from 'react-dom';
-import { Table } from '@uda/bento-components';
+import { Table, IconGlobal } from '@uda/bento-components';
+import { boolean, select } from '@storybook/addon-knobs'
 
-rowData=[]
+const decoratorStyles = {
+  padding: '2rem'
+};
 
-const cols = [{ headerName: "precio", field: "precio", align: "center" } , {aquiotracol}]
+const columnDefs = [
+  {
+    headerName: (
+      <>
+        <IconGlobal />
+        Make
+      </>
+    ),
+    field: 'make',
+    sortable: true,
+    variant: 'primary'
+  },
+  {
+    headerName: 'Model',
+    align: 'center',
+    field: 'model',
+    sortable: true,
+    highlight: true,
+    variant: 'primary'
+  },
+  {
+    headerName: 'Price',
+    field: 'price',
+    sortable: true,
+    align: 'right',
+    variant: 'secondary'
+  }
+];
+const rowData = [
+  { make: 'Toyota', model: 'Celica', price: 35000 },
+  { make: 'Ford', model: 'Fiesta', price: 32000 },
+  { make: 'Porsche', model: '911', price: 72000 },
+  { make: 'Mitsubishi', model: 'Pajero', price: 22000 }
+];
 
 const Sample = () => (
     
-    <div>
-      <Table columns={cols} domLayout="autoHeight" />
+    <div style = {decoratorStyles}>
+      <Table columnsDef={columnDefs} 
+        rows={rowData}
+        domLayout="autoHeight" />
     </div>
   );
 
